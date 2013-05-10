921721a7fe0c5811b36033334d608fe350def60d
Ben Wadsworth <wadben@gmail.com>
2013-05-10 18:54:14 +0100

    Initial Commit

---
+++ Sedimentary/BWAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BWAppDelegate.h
+//  Sedimentary
+//
+//  Created by Ben Wadsworth on 10/05/2013.
+//  Copyright (c) 2013 Ben Wadsworth. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface BWAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ Sedimentary/BWAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  BWAppDelegate.m
+//  Sedimentary
+//
+//  Created by Ben Wadsworth on 10/05/2013.
+//  Copyright (c) 2013 Ben Wadsworth. All rights reserved.
+//
+
+#import "BWAppDelegate.h"
+
+@implementation BWAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
